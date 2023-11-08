bf2a20f94d506aeb34da6721f5e48f23360bdf56
Edgaras Valincius <edval@edgarass-air.mynet>
2023-11-08 08:47:50 +0000

    #1 initialize project

---
+++ src/main/java/com/evalincius/cablogservice/CaBlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.evalincius.cablogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaBlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaBlogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evalincius/cablogservice/CaBlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evalincius.cablogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaBlogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
